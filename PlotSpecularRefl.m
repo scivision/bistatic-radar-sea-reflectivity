a933f4c524600e2132b25ca54069974bdcdc413b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-12-26 14:51:35 -0500

    table1

--- PlotSpecularRefl.m
+++ PlotSpecularRefl.m
@@ -4,19 +4,22 @@ f0 = 10e9; % per pg. 4 of Report
 lambda = c/f0; % m
 
 seastate = ['0','2','3','5','6'];
-sigmaH = [0,0.08,0.2,2.5,4]; 
+sigmaH = [0.01, 0.11, 0.29, 1.03, 1.61]; % Table 1 of Report for SS 0,2,3,5,6
 
 gamma = 0:0.01:6; 
 
-figure(1)
+
+%% Figure 2 of Report
+% doesn't quite match  Figure 2.
+figure(2), clf(2)
 ax = axes('nextplot','add');
 grid(ax,'on')
 i = 1;
 for s = sigmaH
   Rhos = specular_reflection(s,gamma,lambda);
-  plot(ax,gamma,Rhos,'linewidth',2,'DisplayName',seastate(i))
+  plot(ax,gamma,Rhos,'DisplayName',seastate(i))
   i = i+1;
 end
 
 legend(ax,'show')
-title(ax,'Specular rReflectivity vs. grazing angle and sea state')
+title(ax,'Specular Reflectivity vs. grazing angle and sea state')
--- specular_reflection.m
+++ specular_reflection.m
@@ -4,6 +4,9 @@ function RhoS = specular_reflection(sigmaH,gamma,lambda)
   % sigmaH: RMS wave height (meters)
   % lambda: radar wavelength  (meters)
   % gamma: grazing angle (deg)
+  %
+  % https://en.wikipedia.org/wiki/Wave_height
+  
   RhoS = exp(-1/2 * ((4*pi*sigmaH.*sind(gamma))./lambda).^2);
   
 end  % function
