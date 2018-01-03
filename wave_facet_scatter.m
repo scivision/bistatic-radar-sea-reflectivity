a60059f1f81f1c55c29cd732a8e291e7285ecd9d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-01-02 20:57:51 -0500

    eqn2,3

--- wave_facet_scatter.m
+++ wave_facet_scatter.m
@@ -1,31 +1,14 @@
 function [sigmaCoPol, sigmaXPol] = wave_facet_scatter(P, ShadowFactor);
 
-c = 299792458; % m/s
-
-% Electrical properties of sea water
-lambda = c / (P.FGHz*1e9); % [m]
-epsrc = 80 - 1j * 60 * lambda * 4; % complex reflection coeff
-murc = 1; % permittivity
-YEarth = sqrt(epsrc/murc);
-
 % eqn 12: bisector angle
-tanbeta = sqrt(sin(P.theta1)^2 - 2 * sin(P.theta1) * sin(P.theta2) * cos(P.totAngle) + sin(P.theta2)^2)...
-          / (cos(P.theta1) + cos(P.theta2));
+% FIXME different by 90 degrees from eqn 12?
+tanbeta = sqrt(sin(P.theta1).^2 - 2 .* sin(P.theta1) .* sin(P.theta2) .* cos(P.totAngle) + sin(P.theta2).^2)...
+          ./ (cos(P.theta1) + cos(P.theta2));
 
 
-temp1 = (1+tanbeta^2)^2/(P.tanbeta0^2) * exp(-(tanbeta/P.tanbeta0)^2);
+temp1 = (1+tanbeta.^2).^2 ./ P.tanbeta0.^2 .* exp(-(tanbeta ./ P.tanbeta0).^2);
 temp1 = temp1 * ShadowFactor;
-%% Calculate alpha -- to include Polarization
-alpha = eqn17(P);
 
-% dummy variable for Eqn 18.
-Alpha = pi/2 - alpha;
-
-if strcmpi(P.TxPol,'H')
-  RHoriz = (sin(Alpha) - sqrt(YEarth^2 - cos(Alpha)^2)) / (sin(Alpha) + sqrt(YEarth^2 - cos(Alpha)^2));
-elseif strcmpi(P.TxPol,'V')
-  RVert = (YEarth^2 * sin(Alpha) - sqrt(YEarth^2 - cos(Alpha)^2)) / (YEarth^2 * sin(Alpha) + sqrt(YEarth^2 - cos(Alpha)^2));
-end
 
 cosX = cos(P.theta1) * cos(P.theta2) - sin(P.theta1) * sin(P.theta2) * cos(P.totAngle);
 sinX = sqrt(1 - cosX^2);
@@ -34,14 +17,45 @@ sinbeta2 = sin(P.theta2) * sin(P.totAngle) / sinX;
 cosbeta1 = (sin(P.theta2) * cos(P.theta1) + cos(P.theta2) * sin(P.theta1) * cos(P.totAngle))/sinX;
 cosbeta2 = (sin(P.theta1) * cos(P.theta2) + cos(P.theta1) * sin(P.theta2) * cos(P.totAngle))/sinX;
 
-if strcmpi(P.TxPol,'H')
-  sigmaCoPol =temp1 * real((abs(RHoriz) * cosbeta1 * cosbeta2)^2);
-  sigmaXPol = temp1 * real((abs(RHoriz) * cosbeta1 * sinbeta2)^2);
-elseif strcmpi(P.TxPol,'V')
-  sigmaCoPol = temp1 * real((abs(RVert) * cosbeta1 * cosbeta2)^2);
-  sigmaXPol = temp1 * real((abs(RVert) * cosbeta1 * sinbeta2)^2);
+%% Calculate alpha -- to include Polarization
+alpha = eqn17(P);
+
+switch P.TxPol
+  case {'h','H'}
+    rho0H = eqn2(P, alpha);
+    sigmaCoPol =temp1 * real((abs(rho0H) * cosbeta1 * cosbeta2)^2);
+    sigmaXPol = temp1 * real((abs(rho0H) * cosbeta1 * sinbeta2)^2);
+  case {'v','V'}
+    rho0V = eqn3(P, alpha);
+    sigmaCoPol = temp1 * real((abs(rho0V) * cosbeta1 * cosbeta2)^2);
+    sigmaXPol = temp1 * real((abs(rho0V) * cosbeta1 * sinbeta2)^2);
 end
 
+end % function
+
+function rho0H = eqn2(P, alpha)
+  % Fresnel reflection coefficient: horizontal polarization
+  
+  Y = sea_permittivity(P.FGHz);
+  
+  Alpha = pi/2 - alpha;
+  
+  rho0H = (sin(Alpha) - sqrt(Y.^2 - cos(Alpha)^2)) ./ ...
+          (sin(Alpha) + sqrt(Y.^2 - cos(Alpha)^2));
+  
+end % function
+
+
+function rho0V = eqn3(P, alpha)
+  % Fresnel reflection coefficient: vertical polarization
+  
+  Y = sea_permittivity(P.FGHz, alpha);
+  
+  Alpha = pi/2 - alpha;
+  
+  rho0V = (Y.^2 * sin(Alpha) - sqrt(Y.^2 - cos(Alpha).^2)) ./ ...
+          (Y.^2 * sin(Alpha) + sqrt(Y.^2 - cos(Alpha).^2));
+  
 end % function
 
 
@@ -50,4 +64,20 @@ function alpha = eqn17(P)
 alpha = acos(sqrt(1 - cos(P.gammaR)*cos(P.gammaT)*cos(P.totAngle) + sin(P.gammaR)*sin(P.gammaT)) / ...
              sqrt(2));  
   
-end
+end % function
+
+
+function Y = sea_permittivity(FreqGHz)
+  % Eqn 5.
+  
+c = 299792458; % m/s
+epsr = 80; % seawater
+sigmac = 4; % seawater conductivity Siemens
+
+% Electrical properties of sea water
+lambda = c ./ (FreqGHz*1e9); % [m]
+epsrc = epsr- 1j * 60 * lambda * sigmac; % complex reflection coeff
+murc = 1; % permittivity
+Y = sqrt(epsrc / murc);
+  
+end % function
