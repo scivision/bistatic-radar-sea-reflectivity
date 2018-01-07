225275deae2adf56ebf6e418c83532455388450e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-01-07 15:05:44 -0500

    init

---
+++ Figure10.m
@@ -0,0 +1,21 @@
+% Figure 10, page 18
+% No shadowing
+% No wide angle scatter
+% 10 km downrange x 300 m crossrange patch
+% 2 x 2 deg. beamwidth
+
+clear
+
+P = struct('Shadowing','y','Re',8500e3,...
+           'xPatch',50,'yPatch',0,...
+           'D',10e3,'TxPol','V','FGHz',3,
+           'hR',20,'SeaState',3);
+           
+
+%% iterate
+P = grazing_angles(P);
+
+%% Iterate over all directions to patch
+
+%% apply beam tapering and sum returns
+
