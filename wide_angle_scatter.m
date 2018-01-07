fc9ff73bfe0b7df7886bb5a0187e561d38d815bf
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-01-07 18:11:39 -0500

    eqn21-22

--- wide_angle_scatter.m
+++ wide_angle_scatter.m
@@ -1,6 +1,14 @@
 function [sigmaCoPol, sigmaXPol] = wide_angle_scatter(P, sigmaCoPol, sigmaXPol)
 
-  %Have to include Monostatic for wide angle scattering
+  sigmaMdb = eqn21(P);
+
+  [sigmaCoPol, sigmaXPol] = eqn22(sigmaMdb,sigmaCoPol,sigmaXPol);
+  
+end
+
+
+function sigma = eqn21(P)
+%Have to include Monostatic for wide angle scattering
   %     if xPatch < 0
   %         graz = grazRx;
   %     else
@@ -8,21 +16,28 @@ function [sigmaCoPol, sigmaXPol] = wide_angle_scatter(P, sigmaCoPol, sigmaXPol)
   %Choose the smaller of the two grazing angles
   %     end;
   
-%% Table 2
+  
+  %% Table 2
   if strcmpi(P.TxPol,'H')
     CC1 = -73.0;  CC2 = 20.781; CC3= 7.351; CC4=25.65; CC5 = 0.0054;
   elseif strcmpi(P.TxPol,'V')
     CC1 = -50.796; CC2 = 25.93; CC3 = 0.7093; CC4 = 21.588; CC5 = 0.00211;
   end
-
-%% Eqn 21
-  sigma_M_db = CC1 + CC2 * log10(sind(graz))...
-    + (27.5 + CC3 * graz) * log10(P.FGHz) ./ (1.0 + 0.95 * graz)...
-    + CC4 * (P.SeaState + 1) .^ (1./(2.0 + 0.085 * graz + 0.033 * P.SeaState))...
+  
+  sigma = CC1 ...
+    + CC2 * log10(sind(graz)) ...
+    + (27.5 + CC3 * graz) * log10(P.FGHz) ./ (1.0 + 0.95 * graz) ...
+    + CC4 * (P.SeaState + 1) .^ (1./(2.0 + 0.085 * graz + 0.033 * P.SeaState)) ...
     + CC5 * graz.^2;
-%% Eqn 22
-  sigma_M = 10.^(sigma_M_db./10.);
+  
+end
+
+
+function [sigmaCoPol, sigmaXPol] = eqn22(sigmaMdb,sigmaCoPol,sigmaXPol)
+  
+  sigma_M = 10.^(sigmaMdb ./ 10.);
   sigmaCoPol = max(sigmaCoPol, sigma_M);
   
   sigmaXPol = max(sigmaXPol, 0.);
+  
 end
