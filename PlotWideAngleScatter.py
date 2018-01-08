979686cacf681c870c1938552a5d0f48ab565cd2
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-01-08 02:14:11 -0500

    init

---
+++ PlotWideAngleScatter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""Example of calling Matlab code from Python using Oct2Py"""
+from oct2py import Oct2Py
+import numpy as np
+from matplotlib.pyplot import figure,show
+
+
+def plot_was():
+    P = {'grazRx':2.0,
+         'grazTx':np.arange(0.1,10,.1),
+         'TxPol':'V',
+         'FGHz':1.5,
+         'SeaState':3}
+
+    with Oct2Py() as oc:
+        sigmaCo= oc.wide_angle_scatter(P, 0., 0.)
+
+    sigmaCo = sigmaCo.squeeze()
+# %%
+    ax = figure().gca()
+    ax.plot(P['grazTx'], 10*np.log10(sigmaCo), label='co')
+    ax.set_ylabel('$\sigma^0_{VV}$')
+    ax.set_xlabel('Transmit grazing angle [deg]')
+    ax.set_title(f'Wide Angle Scatter, {P["FGHz"]} GHz')
+
+if __name__ == '__main__':
+    plot_was()
+
+    show()
